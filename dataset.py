e87dfa4a1d02a35c18cbea3089711faec46b11a0
Palaash Goel <goelpalaash@gmail.com>
2025-07-01 23:04:06 +0530

    Removed dependency on KG_xyz.pkl files

--- dataset.py
+++ dataset.py
@@ -14,7 +14,6 @@ class MOREPlusDataset(Dataset):
         DC_file,
         O_file,
         OC_file,
-        adj_file,
         path_to_images,
         tokenizer,
         image_transform,
@@ -46,6 +45,87 @@ class MOREPlusDataset(Dataset):
         self.image_transform = image_transform
         self.CFG = CFG
 
+        #Returns adjacency matrix of KG for sample with index = i
+    def construct_kg(self, idx):
+        row = self.data.iloc[idx, :]
+
+        #Used to index all other info
+        pid = row['pid']
+        caption = row['text']
+        caption = caption.split(' ')
+        target = row['target_of_sarcasm']
+
+        #C + CC + D + DC + O + OC
+        adj = np.diag([1.0] * 256)
+        N = 0  #Track current number of tokens
+
+        #C - Caption
+        for j in range(len(caption) - 1):
+          adj[j][j+1] = 1
+          adj[j+1][j] = 1
+        N += len(caption)
+
+        #CC - Caption concepts 
+        if (pid in self.CC):
+          concept_dict = self.CC[pid]
+          concept_keys = list(concept_dict.keys())
+
+          for j in range(len(caption)):
+            word = caption[j]
+            if (word in concept_keys):
+              adj[j][N + concept_keys.index(word)] = concept_dict[word]['weight'] 
+              adj[N + concept_keys.index(word)][j] = concept_dict[word]['weight']
+
+          N += len(concept_keys)
+
+
+        #D - Image description
+        if (pid in self.D):
+          img_caption_start = N
+          img_caption_tokens = self.D[pid].split(' ')
+
+          #Connecting adjacent tokens - WORKS
+          for j in range(N, N + len(img_caption_tokens)-1):
+            adj[j][j+1] = 1
+            adj[j+1][j] = 1
+          N += len(img_caption_tokens)
+
+          #Connecting img desc tokens to concept tokens - WORKS
+          if (pid in self.DC):
+            ic_concept_dict = self.DC[pid]
+            ic_concept_keys = list(ic_concept_dict.keys())
+            for j in range(len(img_caption_tokens)):
+              word = img_caption_tokens[j]
+              if (word in ic_concept_keys):
+
+                adj[img_caption_start + j][N + ic_concept_keys.index(word)] = ic_concept_dict[word]['weight'] 
+                adj[N + ic_concept_keys.index(word)][img_caption_start + j] = ic_concept_dict[word]['weight']
+            N += len(ic_concept_keys)
+
+        #O - Image obj
+        if (pid in self.O):
+          img_obj_start = N
+          img_obj_tokens = self.O[pid]['classes']
+          img_obj_conf_scores = self.O[pid]['confidence_scores']
+
+          N += len(img_obj_tokens)
+            
+        #OC - Image obj concepts
+          if (pid in self.OC):
+            io_concept_dict = self.OC[pid]
+            io_concept_keys = list(io_concept_dict.keys())
+
+            for j in range(len(img_obj_tokens)):
+              word = img_obj_tokens[j]
+
+              if (word in io_concept_keys):
+                adj[img_obj_start + j][N + io_concept_keys.index(word)] = io_concept_dict[word]['weight']
+                adj[N + io_concept_keys.index(word)][img_obj_start + j] = io_concept_dict[word]['weight']
+
+            N += len(io_concept_keys)
+
+        return np.float32(adj)
+    
     def __getitem__(self, idx):
 
         row = self.data.iloc[idx, :]
@@ -53,11 +133,7 @@ class MOREPlusDataset(Dataset):
         pid = row["pid"]
         caption = row["text"]
         text = caption.split(" ")
-        
-        if pid in self.adj:
-            adj = self.adj[pid]
-        else:
-            adj = np.float32(np.diag([1.0] *256))
+        adj = self.construct_kg(idx)
         
         #[Section 4.3] Concat caption concept tokens
         if pid in self.CC:
